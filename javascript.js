9f48d5133f6b3d96bf882a7b717b22af6c85692c
LaithWazani <Laithwaza1@gmail.com>
2021-03-24 09:05:52 +0200

    new javascript

---
+++ javascript.js
@@ -0,0 +1,25 @@
+var num=40;
+while (num<100) {
+   num = num+10;
+   console.log(num);
+   
+}
+if (num==100) {
+    alert(" go big");
+    
+}else {
+    alert("try hard");
+}
+alert("that's great that you could get to here");
+var namper =  prompt("please enter you name 1");
+var x=2;
+for (var i = 0; i < 10; i++) {
+    
+x= x * 2;
+
+console.log(x);
+
+    
+}
+
+
